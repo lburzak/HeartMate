d92a45b9b3c6581946cbc57245faf3d3efbac4b3
jakubdubrowski <jakub.dubro@gmail.com>
2022-11-03 16:29:20 +0100

    początki UI

--- lib/profile/profile_view.dart
+++ lib/profile/profile_view.dart
@@ -3,11 +3,77 @@ import 'package:flutter/material.dart';
 
 class ProfileView extends StatelessWidget {
   final ProfileState state;
+  int weight = 0;
+  int age = 0;
+  int height = 0;
+  String gender = "";
 
-  const ProfileView({super.key, required this.state});
+  ProfileView({super.key, required this.state});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Profile");
+    return Form(
+      child: Align(
+        alignment: Alignment.topCenter,
+        child: SafeArea(
+          left: true,
+          top: true,
+          right: true,
+          bottom: true,
+          minimum: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Wprowadź wiek',
+                  border: UnderlineInputBorder(),
+                  filled: true,
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Wprowadź wagę',
+                  border: UnderlineInputBorder(),
+                  filled: true,
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Wprowadź wzrost',
+                  border: UnderlineInputBorder(),
+                  filled: true,
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Wprowadź płeć',
+                  border: UnderlineInputBorder(),
+                  filled: true,
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Wprowadź dolny zakres INR',
+                  border: UnderlineInputBorder(),
+                  filled: true,
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Wprowadź górny zakres INR',
+                  border: UnderlineInputBorder(),
+                  filled: true,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
