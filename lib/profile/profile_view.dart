bd178c8f73962c6146e3faf18aba46a5b8647280
jakubdubrowski <jakub.dubro@gmail.com>
2022-11-04 17:52:01 +0100

    dodanie genderList

--- lib/profile/profile_view.dart
+++ lib/profile/profile_view.dart
@@ -1,13 +1,16 @@
+import 'package:apkainzynierka/profile/gender.dart';
 import 'package:apkainzynierka/profile/profile_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProfileView extends StatelessWidget {
   final ProfileState state;
+  final List<String> genderList = <String>['mężczyzna', 'kobieta'];
   int weight = 0;
   int age = 0;
   int height = 0;
   String gender = "";
+  String? dropdownValue;
 
   ProfileView({super.key, required this.state});
 
@@ -61,15 +64,38 @@ class ProfileView extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 10),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Wprowadź płeć',
-                    border: UnderlineInputBorder(),
-                    filled: true,
-                  ),
+                Row(
+                  children: [
+                    const Text("Wybierz płeć: "),
+                    DropdownButton<String>(
+                      value: context.select((ProfileCubit cubit) =>
+                          cubit.state.gender == Gender.male
+                              ? "mężczyzna"
+                              : "kobieta"),
+                      icon: const Icon(Icons.arrow_drop_down),
+                      elevation: 16,
+                      style: const TextStyle(color: Colors.blue),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.blue,
+                      ),
+                      onChanged: (String? value) {
+                        context.read<ProfileCubit>().setGender(
+                            value == "mężczyzna" ? Gender.male : Gender.female);
+                      },
+                      items: genderList
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ],
                 ),
                 const SizedBox(height: 10),
                 TextFormField(
+                  keyboardType: TextInputType.number,
                   decoration: const InputDecoration(
                     hintText: 'Wprowadź dolny zakres INR',
                     border: UnderlineInputBorder(),
@@ -78,6 +104,7 @@ class ProfileView extends StatelessWidget {
                 ),
                 const SizedBox(height: 10),
                 TextFormField(
+                  keyboardType: TextInputType.number,
                   decoration: const InputDecoration(
                     hintText: 'Wprowadź górny zakres INR',
                     border: UnderlineInputBorder(),
