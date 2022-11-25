be888208500429b7532ff66db551a3a363d845d2
jakubdubrowski <jakub.dubro@gmail.com>
2022-11-25 18:14:49 +0100

    widget dzisiejszej dawki

--- lib/today_dosage/today_dosage_view.dart
+++ lib/today_dosage/today_dosage_view.dart
@@ -2,6 +2,7 @@ import 'package:apkainzynierka/common/navigation/navigation_handler.dart';
 import 'package:apkainzynierka/today_dosage/state/today_dosage_state.dart';
 import 'package:apkainzynierka/today_dosage/today_dosage_cubit.dart';
 import 'package:apkainzynierka/today_dosage/today_dosage_router.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart' hide Router;
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -19,14 +20,43 @@ class _TodayDosageViewState extends State<TodayDosageView>
   @override
   Widget build(BuildContext context) {
     final cubit = context.read<TodayDosageCubit>();
-
+    const state = TodayDosageState(
+        taken: true, potency: 2, custom: false, scheduleUndefined: true);
+    //const state = widget.state;
     return SizedBox(
-      height: 120,
-      child: ElevatedButton(
-          child: Text(
-              "Today dosage: ${widget.state.taken ? "taken" : "not taken"}"),
-          onLongPress: () => cubit.showCustomDosageScreen(),
-          onPressed: () => cubit.toggleTaken()),
-    );
+        height: 120,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: DottedBorder(
+            color: state.taken ? Colors.transparent : Colors.white,
+            strokeCap: StrokeCap.round,
+            dashPattern: const [8, 8],
+            strokeWidth: state.taken ? 0 : 4,
+            child: SizedBox.expand(
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: getBackgroundColor(),
+                    elevation: 0
+                  ),
+                  onLongPress: () => cubit.showCustomDosageScreen(),
+                  onPressed: () => cubit.toggleTaken(),
+                  child: Text("Dzisiejsza dawka\n\n"
+                      " ${state.taken ? "Przyjęto dawkę" : "Przyjmij dawkę"}"
+                      " ${state.potency}"
+                      " mg")),
+            ),
+          ),
+        ));
+  }
+
+  Color getBackgroundColor() {
+    if (widget.state.scheduleUndefined) {
+      return Colors.amber;
+    }
+
+    if (widget.state.taken) {
+      return Colors.green;
+    }
+    return Colors.transparent;
   }
 }
