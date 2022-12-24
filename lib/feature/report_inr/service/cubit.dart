1f9b024393c9d07d844c31de373b53e82e2db428
polydome <polydome@outlook.com>
2022-12-24 14:48:03 +0100

    [report-inr] add redirect after successful submit

--- lib/feature/report_inr/service/cubit.dart
+++ lib/feature/report_inr/service/cubit.dart
@@ -1,11 +1,13 @@
 import 'package:apkainzynierka/feature/report_inr/model/state.dart';
+import 'package:apkainzynierka/feature/report_inr/service/routing.dart';
 import 'package:apkainzynierka/feature/report_inr/usecase/report_inr.dart';
 import 'package:bloc/bloc.dart';
 
 class ReportInrCubit extends Cubit<ReportInrState> {
   final ReportInr _reportInr;
+  final ReportInrRouting _routing;
 
-  ReportInrCubit(this._reportInr)
+  ReportInrCubit(this._reportInr, this._routing)
       : super(const ReportInrState(inr: null, error: null));
 
   void setInr(double value) {
@@ -19,8 +21,10 @@ class ReportInrCubit extends Cubit<ReportInrState> {
 
   void submit() {
     final inr = state.inr;
+
     if (inr != null) {
       _reportInr(inr);
+      _routing.finish();
     }
   }
 }
