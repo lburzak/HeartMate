04063e0e6088b8b66dbc5b2cb414505b8e72f666
polydome <polydome@outlook.com>
2023-01-04 21:34:38 +0100

    [arch] update freezed models

--- lib/domain/model/schedule.freezed.dart
+++ lib/domain/model/schedule.freezed.dart
@@ -1,7 +1,7 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
-// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'schedule.dart';
 
@@ -133,6 +133,7 @@ class _$_Schedule implements _Schedule {
   final List<double> _dosageScheme;
   @override
   List<double> get dosageScheme {
+    if (_dosageScheme is EqualUnmodifiableListView) return _dosageScheme;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(_dosageScheme);
   }
--- lib/profile/state/profile_state.freezed.dart
+++ lib/profile/state/profile_state.freezed.dart
@@ -1,7 +1,7 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
-// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'profile_state.dart';
 
@@ -288,6 +288,8 @@ class _$_ProfileState implements _ProfileState {
   final List<String> _availableMedicines;
   @override
   List<String> get availableMedicines {
+    if (_availableMedicines is EqualUnmodifiableListView)
+      return _availableMedicines;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(_availableMedicines);
   }
@@ -309,6 +311,7 @@ class _$_ProfileState implements _ProfileState {
   final List<String> _otherMedicines;
   @override
   List<String> get otherMedicines {
+    if (_otherMedicines is EqualUnmodifiableListView) return _otherMedicines;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(_otherMedicines);
   }
