d246769dd98dfb7f25a49571b739ad54b501f2b2
polydome <polydome@outlook.com>
2023-01-04 22:16:24 +0100

    [domain,data] add `existsScheduleForDay` method

--- lib/domain/repository/schedule_repository.dart
+++ lib/domain/repository/schedule_repository.dart
@@ -1,6 +1,8 @@
 abstract class ScheduleRepository {
   int? getScheduleIdForDay(DateTime dateTime);
 
+  bool existsScheduleForDay(DateTime dateTime);
+
   double getDosageForDay(int scheduleId, DateTime dateTime);
 
   void createSchedule(
