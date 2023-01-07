04f0aa006cb3b1fa22c33d64a1a707c9f7c99388
polydome <polydome@outlook.com>
2023-01-07 10:43:57 +0100

    [main] remove unused container widget

--- lib/feature/welcome/ui/welcome_view.dart
+++ lib/feature/welcome/ui/welcome_view.dart
@@ -7,62 +7,58 @@ class WelcomePageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Stack(
-          children: [
-            Positioned(
-              bottom: 360,
-              left: 40,
-              child: Text(
-                "Witaj!",
-                style: GoogleFonts.balooDa2(
-                  textStyle: const TextStyle(fontSize: 32.0),
-                ),
+    return Stack(
+      children: [
+        Positioned(
+          bottom: 360,
+          left: 40,
+          child: Text(
+            "Witaj!",
+            style: GoogleFonts.balooDa2(
+              textStyle: const TextStyle(fontSize: 32.0),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 300,
+          left: 40,
+          child: SizedBox(
+            width: 250,
+            child: Text(
+              "Przed rozpoczęciem korzystania z aplikacji uzupełnij swój profil.",
+              style: GoogleFonts.balooDa2(
+                textStyle: const TextStyle(color: Colors.grey, fontSize: 16.0),
               ),
             ),
-            Positioned(
-              bottom: 300,
-              left: 40,
-              child: SizedBox(
-                width: 250,
-                child: Text(
-                  "Przed rozpoczęciem korzystania z aplikacji uzupełnij swój profil.",
-                  style: GoogleFonts.balooDa2(
-                    textStyle:
-                        const TextStyle(color: Colors.grey, fontSize: 16.0),
-                  ),
-                ),
+          ),
+        ),
+        const Positioned(
+          bottom: 215,
+          left: 40,
+          child: Icon(Icons.info_outline),
+        ),
+        Positioned(
+          bottom: 200,
+          left: 80,
+          child: SizedBox(
+            width: 300,
+            child: Text(
+              "Ustawienia profilu możesz edytować w dowolnej chwili w zakładce \"Profil\".",
+              style: GoogleFonts.balooDa2(
+                textStyle: const TextStyle(fontSize: 16.0),
               ),
             ),
-            const Positioned(
-              bottom: 215,
-              left: 40,
-              child: Icon(Icons.info_outline),
-            ),
-            Positioned(
-              bottom: 200,
-              left: 80,
-              child: SizedBox(
-                width: 300,
-                child: Text(
-                  "Ustawienia profilu możesz edytować w dowolnej chwili w zakładce \"Profil\".",
-                  style: GoogleFonts.balooDa2(
-                    textStyle: const TextStyle(fontSize: 16.0),
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 40,
-              right: 40,
-              child: IconButton(
-                  color: Colors.blue,
-                  icon: const Icon(Icons.arrow_forward),
-                  onPressed: () => context.go("/profile/editor")),
-            ),
-          ],
-        ));
+          ),
+        ),
+        Positioned(
+          bottom: 40,
+          right: 40,
+          child: IconButton(
+              color: Colors.blue,
+              icon: const Icon(Icons.arrow_forward),
+              onPressed: () => context.go("/profile/editor")),
+        ),
+      ],
+    );
   }
 }
