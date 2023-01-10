95e53fc25b9e0f18c1e88ce16390ac0454c6f08a
polydome <polydome@outlook.com>
2023-01-10 21:03:14 +0100

    [journal] add inr info fetching

--- lib/feature/journal/service/cubit.dart
+++ lib/feature/journal/service/cubit.dart
@@ -1,4 +1,7 @@
+import 'package:apkainzynierka/domain/model/inr_measurement.dart';
 import 'package:apkainzynierka/domain/repository/dose_repository.dart';
+import 'package:apkainzynierka/domain/repository/inr_measurement_repository.dart';
+import 'package:apkainzynierka/domain/repository/profile_repository.dart';
 import 'package:apkainzynierka/domain/repository/schedule_repository.dart';
 import 'package:apkainzynierka/feature/journal/model/day_summary.dart';
 import 'package:apkainzynierka/feature/journal/model/state.dart';
@@ -7,8 +10,11 @@ import 'package:bloc/bloc.dart';
 class JournalCubit extends Cubit<JournalState> {
   final DoseRepository _doseRepository;
   final ScheduleRepository _scheduleRepository;
+  final InrMeasurementRepository _inrMeasurementRepository;
+  final ProfileRepository _profileRepository;
 
-  JournalCubit(this._doseRepository, this._scheduleRepository)
+  JournalCubit(this._doseRepository, this._scheduleRepository,
+      this._inrMeasurementRepository, this._profileRepository)
       : super(const JournalState());
 
   void onDaySelected(DateTime day) {
@@ -19,9 +25,28 @@ class JournalCubit extends Cubit<JournalState> {
         : null;
     final dose = _doseRepository.findDoseForDay(day);
 
-    final summary =
-        DaySummary(dosage: dose?.potency ?? dosage, taken: dose != null);
+    final inrMeasurement = _inrMeasurementRepository.findForDay(day);
+
+    final summary = DaySummary(
+        dosage: dose?.potency ?? dosage,
+        taken: dose != null,
+        inr: inrMeasurement?.inr,
+        inrRating: inrMeasurement != null ? getRating(inrMeasurement) : null);
 
     emit(state.copyWith(selectedDay: day, selectedDaySummary: summary));
+  }
+
+  InrRating getRating(InrMeasurement inrMeasurement) {
+    final profile =
+        _profileRepository.findForDateTime(inrMeasurement.reportDate);
+    if (inrMeasurement.inr < profile.inrRange.from) {
+      return InrRating.tooLow;
+    }
+
+    if (inrMeasurement.inr > profile.inrRange.to) {
+      return InrRating.tooHigh;
+    }
+
+    return InrRating.balanced;
   }
 }
--- lib/util/time_extensions.dart
+++ lib/util/time_extensions.dart
@@ -3,3 +3,8 @@ extension IntTime on int {
 
   bool get isValidMinute => this < Duration.minutesPerHour && !isNegative;
 }
+
+extension Days on DateTime {
+  bool isSameDayAs(DateTime b) =>
+      year == b.year && month == b.month && day == b.day;
+}
