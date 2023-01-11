15f12555d767412e83251677f3fa4da8cb3c5031
Jakub Dubrowski <jakub.dubro@gmail.com>
2023-01-11 16:28:19 +0100

    wip widok szczegółów dnia w dzienniku

--- lib/feature/journal/ui/day_summary_view.dart
+++ lib/feature/journal/ui/day_summary_view.dart
@@ -11,12 +11,67 @@ class DaySummaryView extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(model.dosage.toString()),
-        Text(model.taken.toString()),
-        Text(model.anticoagulant.toString()),
-        Text(model.otherMedicines.toString()),
-        Text(model.inr.toString()),
-        Text(model.inrRating.toString()),
+        // model.taken ? Text("data") : Text("data2"),
+        Row(children: [
+          const SizedBox(width: 20,),
+          const Text("Dawka"),
+          const SizedBox(width: 57,),
+          Text("${model.dosage.toString()} mg"),
+          const SizedBox(width: 80,),
+          const Chip(
+            avatar: CircleAvatar(
+              backgroundColor: Color(0xFF255500),
+              child: Icon(
+                Icons.done,
+                color: Color(0xFF62DF00),
+              ),
+            ),
+            label: Text('Przyjęta', style: TextStyle(color: Color(0xFF62DF00))),
+            backgroundColor: Color(0xFF255500),
+          )
+        ]),
+        //Text(model.taken.toString()),
+        Row(children: [
+          const SizedBox(width: 20,),
+          const Text("Antykoagulant"),
+          const SizedBox(width: 10,),
+          Text(model.anticoagulant.toString().substring(14)),
+        ]),
+        //Text("Antykoagulant ${model.anticoagulant.toString().substring(14)}"),
+        const SizedBox(height: 15,),
+        Row(children: [
+          const SizedBox(width: 20,),
+          const Text("Inne leki"),
+          const SizedBox(width: 48,),
+          Text(model.otherMedicines
+              .toString()
+              .replaceAll('[', '')
+              .replaceAll(']', '')),
+        ]),
+        //Text("Inne leki ${model.otherMedicines.toString().replaceAll('[', '').replaceAll(']', '')}"),
+        Row(children: [
+          const SizedBox(width: 20,),
+          const Text("INR"),
+          const SizedBox(width: 77,),
+          Text(model.inr.toString()),
+          const SizedBox(width: 111,),
+          const Chip(
+            avatar: CircleAvatar(
+              backgroundColor: Color(0xFF255500),
+              child: Icon(
+                Icons.done,
+                color: Color(0xFF62DF00),
+              ),
+            ),
+            label: Text('W normie', style: TextStyle(color: Color(0xFF62DF00))),
+            backgroundColor: Color(0xFF255500),
+          )
+        ]),
+        //Text(model.inrRating.toString()),
+        Row(children: const [
+          SizedBox(width: 20,),
+          Text("Notatka"),
+        ],),
       ],
     );
   }
