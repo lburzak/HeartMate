eedb3b75600f7eed4233d087495a0191dfb8dac3
polydome <polydome@outlook.com>
2023-01-22 23:59:49 +0100

    [schedule_wizard] implement anticoagulant-based dosage steps

--- lib/feature/schedule_wizard/service/cubit.dart
+++ lib/feature/schedule_wizard/service/cubit.dart
@@ -5,6 +5,7 @@ import 'package:apkainzynierka/feature/schedule_wizard/model/schedule_type.dart'
 import 'package:apkainzynierka/feature/schedule_wizard/model/schedule_wizard_state.dart';
 import 'package:apkainzynierka/feature/schedule_wizard/service/router.dart';
 import 'package:apkainzynierka/feature/schedule_wizard/usecase/create_schedule.dart';
+import 'package:apkainzynierka/feature/schedule_wizard/usecase/get_step_dosage.dart';
 import 'package:apkainzynierka/util/date.dart';
 import 'package:bloc/bloc.dart';
 import 'package:event_bus/event_bus.dart';
@@ -14,26 +15,35 @@ const _initialScheduleType = ScheduleType.daily;
 class ScheduleWizardCubit extends Cubit<ScheduleWizardState> {
   final ScheduleWizardRouter _router;
   final CreateSchedule _createSchedule;
+  final GetStepDosage _getStepDosage;
   final EventBus _eventBus;
 
-  ScheduleWizardCubit(this._router, this._createSchedule, this._eventBus)
+  ScheduleWizardCubit(
+      this._router, this._createSchedule, this._eventBus, this._getStepDosage)
       : super(ScheduleWizardState(
             scheduleType: _initialScheduleType,
             startDate: Date.today(),
+            dosageStepsIndices: _initStepIndices(_initialScheduleType),
             dosages: _initDosages(_initialScheduleType)));
-
-  double get _dosageStep => 0.25;
 
   void incrementDosage(int dayIndex) {
     emit(state.copyWith(
+        dosageStepsIndices: state.dosageStepsIndices
+            .transformedAt(dayIndex, (item) => item + 1),
         dosages: state.dosages.transformedAt(
-            dayIndex, (currentValue) => max(0, currentValue + _dosageStep))));
+            dayIndex,
+            (currentValue) => max(
+                0, _getStepDosage(state.dosageStepsIndices[dayIndex] + 1)))));
   }
 
   void decrementDosage(int dayIndex) {
     emit(state.copyWith(
+        dosageStepsIndices: state.dosageStepsIndices
+            .transformedAt(dayIndex, (item) => item - 1),
         dosages: state.dosages.transformedAt(
-            dayIndex, (currentValue) => max(0, currentValue - _dosageStep))));
+            dayIndex,
+            (currentValue) => max(
+                0, _getStepDosage(state.dosageStepsIndices[dayIndex] - 1)))));
   }
 
   void setStartDate(DateTime dateTime) {
@@ -45,7 +55,10 @@ class ScheduleWizardCubit extends Cubit<ScheduleWizardState> {
   }
 
   void setScheduleType(ScheduleType type) {
-    emit(state.copyWith(scheduleType: type, dosages: _initDosages(type)));
+    emit(state.copyWith(
+        scheduleType: type,
+        dosages: _initDosages(type),
+        dosageStepsIndices: _initStepIndices(type)));
   }
 
   void save() {
@@ -61,6 +74,15 @@ class ScheduleWizardCubit extends Cubit<ScheduleWizardState> {
   }
 
   static List<double> _initDosages(ScheduleType scheduleType) {
+    switch (scheduleType) {
+      case ScheduleType.daily:
+        return List.generate(1, (index) => 0);
+      case ScheduleType.weekly:
+        return List.generate(7, (index) => 0);
+    }
+  }
+
+  static List<int> _initStepIndices(ScheduleType scheduleType) {
     switch (scheduleType) {
       case ScheduleType.daily:
         return List.generate(1, (index) => 0);
