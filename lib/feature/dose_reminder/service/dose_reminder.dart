401a00bbb3e06585412c0bc2f79d6cd3b7507c30
polydome <polydome@outlook.com>
2023-02-04 23:19:10 +0100

    [dose-reminder] bind data to notification

--- lib/feature/dose_reminder/service/dose_reminder.dart
+++ lib/feature/dose_reminder/service/dose_reminder.dart
@@ -1,3 +1,7 @@
+import 'package:apkainzynierka/data/database.dart';
+import 'package:apkainzynierka/data/local_schedule_repository.dart';
+import 'package:apkainzynierka/domain/repository/schedule_repository.dart';
+import 'package:apkainzynierka/domain/usecase/get_today_dosage.dart';
 import 'package:apkainzynierka/feature/dose_reminder/service/notification_service.dart';
 import 'package:apkainzynierka/main.dart';
 import 'package:kiwi/kiwi.dart';
@@ -8,13 +12,20 @@ const _reminderNotificationChannelDescription =
 
 class DoseReminder {
   final NotificationService _notificationService;
+  final GetTodayDosage _getTodayDosage;
 
-  DoseReminder(this._notificationService);
+  DoseReminder(this._notificationService, this._getTodayDosage);
 
   void trigger() async {
+    final dosage = _getTodayDosage();
+
+    if (dosage == null || dosage == 0) {
+      return;
+    }
+
     _notificationService.show(
         title: "Przyjmij dzisiejszą dawkę!",
-        body: "Zaplanowana dawka: 5 mg",
+        body: "Zaplanowana dawka: $dosage mg",
         channelName: _reminderNotificationChannelName,
         channelDescription: _reminderNotificationChannelDescription);
   }
@@ -22,7 +33,11 @@ class DoseReminder {
 
 class DoseReminderModule extends KiwiContainer {
   DoseReminderModule(AppContainer appContainer) : super.scoped() {
-    registerFactory((r) => DoseReminder(r.resolve()));
+    registerFactory((r) => DoseReminder(r.resolve(), r.resolve()));
     registerInstance(appContainer.resolve<NotificationService>());
+    registerFactory((r) => GetTodayDosage(r.resolve()));
+    registerFactory<ScheduleRepository>(
+        (r) => LocalScheduleRepository(r.resolve()));
+    registerInstance(appContainer.resolve<BoxDatabase>());
   }
 }
--- lib/feature/dose_reminder/service/dose_reminder_scheduler.dart
+++ lib/feature/dose_reminder/service/dose_reminder_scheduler.dart
@@ -1,14 +1,23 @@
+import 'package:apkainzynierka/data/database.dart';
 import 'package:apkainzynierka/feature/dose_reminder/service/dose_reminder.dart';
 import 'package:apkainzynierka/main.dart';
+import 'package:logger/logger.dart';
 import 'package:workmanager/workmanager.dart';
 
 @pragma('vm:entry-point')
 void callbackDispatcher() {
   Workmanager().executeTask((task, inputData) async {
-    final appContainer = AppContainer();
-    final doseReminderModule = DoseReminderModule(appContainer);
-    final doseReminder = doseReminderModule.resolve<DoseReminder>();
-    doseReminder.trigger();
+    try {
+      final appContainer = AppContainer();
+      await BoxDatabase.init();
+      final doseReminderModule = DoseReminderModule(appContainer);
+      final doseReminder = doseReminderModule.resolve<DoseReminder>();
+      doseReminder.trigger();
+    } catch (err) {
+      Logger().e(err.toString());
+      throw Exception(err);
+    }
+
     return true;
   });
 }
@@ -20,7 +29,7 @@ class DoseReminderScheduler {
 
   void schedule(int hour, int minute) {
     _workManager.registerPeriodicTask("thisisveryrandom2", "notify2",
-        frequency: const Duration(seconds: 10));
+        frequency: const Duration(days: 1));
   }
 
   void cancel() {
