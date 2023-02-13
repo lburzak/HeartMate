270b57e17d2da9adcceea192ab42c039ecfc2185
polydome <polydome@outlook.com>
2023-02-13 22:55:58 +0100

    [custom-dosage] improve dialog styling

--- lib/feature/report_inr/ui/view.dart
+++ lib/feature/report_inr/ui/view.dart
@@ -1,3 +1,4 @@
+import 'package:apkainzynierka/feature/profile_editor/ui/view.dart';
 import 'package:apkainzynierka/feature/report_inr/model/state.dart';
 import 'package:apkainzynierka/feature/report_inr/service/cubit.dart';
 import 'package:flutter/material.dart';
@@ -12,11 +13,15 @@ class ReportInrView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(10.0),
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisSize: MainAxisSize.min,
           children: [
-            const Text("Dodaj pomiar"),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Header(text: "Dodaj pomiar"),
+            ),
             const SizedBox(
               height: 10,
             ),
